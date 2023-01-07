dcab9fefe6d580f36f2fd4a915dc1ac7a0af7f3b
dlzagu <eodnsdlekd@naver.com>
2023-01-07 21:47:39 +0900

    feat :  folder structure and basic routing

---
+++ src/customRouter/index.tsx
@@ -0,0 +1,40 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "../components/global/header";
+import ErrorAlert from "../components/errorBoundary/ErrorAlert";
+import ErrorBoundary from "../components/errorBoundary/ErrorBoundary";
+import LoadingCycle from "../components/alert/Loader";
+import Alert from "../components/alert/Alert";
+
+const Main = lazy(() => import("../pages/index"));
+const Login = lazy(() => import("../pages/login"));
+const Register = lazy(() => import("../pages/register"));
+const NotFound = lazy(() => import("../components/global/NotFound"));
+
+export const PATH = {
+  MAIN: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  NOT_FOUND: "/*",
+};
+
+const CustomRouter = () => {
+  return (
+    <Router>
+      <ErrorBoundary fallback={({ error }) => <ErrorAlert error={error} />}>
+        <Header />
+        <Alert />
+        <Suspense fallback={<LoadingCycle />}>
+          <Routes>
+            <Route path={PATH.MAIN} element={<Main />} />
+            <Route path={PATH.LOGIN} element={<Login />} />
+            <Route path={PATH.REGISTER} element={<Register />} />
+            <Route path={PATH.NOT_FOUND} element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </Router>
+  );
+};
+
+export default CustomRouter;
---
+++ src/pages/register.tsx
@@ -0,0 +1,12 @@
+import BasePageComponent from "../hoc/BasePageComponent";
+import AuthRegister from "../components/auth/AuthRegister";
+
+const Register = () => {
+  return (
+    <BasePageComponent>
+      <AuthRegister />
+    </BasePageComponent>
+  );
+};
+
+export default Register;
