69272d9db02987942d462e2a5b8e4174a332a31f
dlzagu <eodnsdlekd@naver.com>
2023-01-08 23:39:05 +0900

    feat : added itemDeleteRequest

--- src/api/todoFetcher.ts
+++ src/api/todoFetcher.ts
@@ -13,3 +13,25 @@ export async function ItemAddRequest(todoForm: TodoFormInitial) {
 
   return res.data;
 }
+
+export async function getTodoList() {
+  const res = await customAxios.get("/todos", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${Storage.getToken()}`,
+    },
+  });
+
+  return res.data.data;
+}
+
+export async function itemDeleteRequest(todoId: string) {
+  const res = await customAxios.delete(`/todos/${todoId}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${Storage.getToken()}`,
+    },
+  });
+
+  return res.data.data;
+}
