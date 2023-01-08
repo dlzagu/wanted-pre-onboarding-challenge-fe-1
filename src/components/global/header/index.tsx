0f1a05e35883557b5f3914353dfeb65767587165
dlzagu <eodnsdlekd@naver.com>
2023-01-08 22:30:51 +0900

    feat : added Header

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,5 +1,73 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "../../../customRouter";
+import Storage from "../../../storage";
+import useSetAlert from "../../../hooks/useSetAlert";
+
 const Header = () => {
-  return <div>Header</div>;
+  const isLogin = Storage.getToken() ? true : false;
+  const navigate = useNavigate();
+  const { setAlertSuccess } = useSetAlert();
+  const hanldeClickLogout = () => {
+    Storage.clearToken();
+    setAlertSuccess({ success: "로그아웃 되었습니다." });
+    navigate(PATH.LOGIN);
+  };
+  return (
+    <HeaderContainer>
+      <ContentContainer>
+        <Logo>TODO!</Logo>
+        {!isLogin ? (
+          <Login onClick={() => navigate(PATH.LOGIN)}>login</Login>
+        ) : (
+          <Logout onClick={hanldeClickLogout}>logout</Logout>
+        )}
+      </ContentContainer>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
+
+const HeaderContainer = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 0 8%;
+  background: none;
+  z-index: 10;
+  transition: all 0.5s;
+`;
+
+const ContentContainer = styled.nav`
+  width: 100%;
+  height: 7rem;
+
+  ${({ theme }) => theme.mixins.flexBox("row", "center", "space-between")}
+  @media (max-width: ${({ theme }) => theme.bpSmallest}) {
+    height: 10rem;
+  }
+`;
+
+const Logo = styled.h3`
+  ${({ theme }) =>
+    theme.mixins.title(theme.fontSemiMedium, theme.weightBold, theme.mainWhite)}
+`;
+
+const Login = styled.div`
+  cursor: pointer;
+  color: ${({ theme }) => theme.mainWhite};
+  transition: all 0.5s;
+  &:hover {
+    color: ${({ theme }) => theme.darkGrey};
+  }
+`;
+
+const Logout = styled.div`
+  cursor: pointer;
+  color: ${({ theme }) => theme.mainWhite};
+  transition: all 0.5s;
+  &:hover {
+    color: ${({ theme }) => theme.darkGrey};
+  }
+`;
