46d7b834e6f72610f6f5e025b730b55148c956df
dlzagu <eodnsdlekd@naver.com>
2023-01-08 23:38:21 +0900

    fix : fixed hook

--- src/hooks/useAddItem.ts
+++ src/hooks/useAddItem.ts
@@ -1,16 +1,16 @@
-import { useMutation } from "react-query";
-import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "react-query";
 import useSetAlert from "./useSetAlert";
 import { ErrorType } from "../types/error";
 import { ItemAddRequest } from "../api/todoFetcher";
 
 export default function useAddItem() {
   const { setAlertSuccess, setAlertError } = useSetAlert();
+  const queryClient = useQueryClient();
 
   const mutation = useMutation(ItemAddRequest, {
     onSuccess: (data) => {
       setAlertSuccess({ success: "작성성공" });
-      console.log(data);
+      queryClient.invalidateQueries(["todoList"]);
     },
     onError: (error: ErrorType) => {
       const errorMessage = error.response.data.message;
