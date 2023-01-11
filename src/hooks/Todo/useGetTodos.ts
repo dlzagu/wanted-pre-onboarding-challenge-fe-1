4ffc8c8c91be8953c24b443cd0ddf473bbed199b
dlzagu <eodnsdlekd@naver.com>
2023-01-11 23:26:05 +0900

    fix : changed custom hook name

--- src/hooks/Todo/useGetTodos.ts
+++ src/hooks/Todo/useGetTodos.ts
@@ -2,6 +2,6 @@ import { getTodoRequest } from "../../api/todoFetcher";
 import { useQuery } from "react-query";
 import { TodoListInitial } from "../../types/todo";
 
-export default function uesGetTodos() {
+export default function useGetTodos() {
   return useQuery<TodoListInitial[], Error>(["todoList"], getTodoRequest);
 }
